c558aa51f5245d18be029f60c71bb026a1c54dd6
Oguzhan <yakusio@googlemail.com>
2023-10-20 19:29:27 +0200

    Add project files.

---
+++ 02_Chapter/FirstClassicProgram/Main.cpp
@@ -0,0 +1,11 @@
+// This program outputs the message "Hello World!" to your screen
+
+#include <iostream>
+
+// C++ programs start by executing the function main
+int main()
+{
+	// output Hello World!
+	std::cout << "Hello World!\n";
+	return 0;
+}
