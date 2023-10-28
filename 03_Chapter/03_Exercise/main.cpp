6f34dfad144ba2a3eabbb94bf4a7c08bb5e72dda
Oguzhan <yakusio@googlemail.com>
2023-10-28 23:13:17 +0200

    Finished excercise 3

---
+++ 03_Chapter/03_Exercise/main.cpp
@@ -0,0 +1,17 @@
+// Write a program that doesn’t do anything, 
+// but declares a number of variables with legal and illegal names (such as int double = 0;), 
+// so that you can see how the compiler reacts.
+
+#include <iostream>
+#include <string>
+int main()
+{
+	int double test = 0;
+	std::string int test2;
+	int main = 4;
+	double std{ a };
+	char 23Ac;
+
+
+	return 0;
+}
