b630311f241c4440454dbfad712d9e055a148769
Oguzhan <yakusio@googlemail.com>
2023-12-20 15:06:58 +0100

    Progress

--- 04_Chapter/18_Exercise/18_Exercise.cpp
+++ 04_Chapter/18_Exercise/18_Exercise.cpp
@@ -4,9 +4,79 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+// Function prototypes
+std::stringstream printMenu();
+std::vector<double> inputValues();
+std::vector<double> outputCalculation(const std::vector<double> values);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << printMenu().str();
+    std::vector<double> inputVector{ inputValues() };
+    if (inputVector.size() == 0)
+    {
+        return 0;
+    }
+    std::vector<double> calculationResult{ outputCalculation(inputVector) };
+    std::cout << "The first result of the calculation is: " << calculationResult.at(0) << " \n The second result is: " << calculationResult.at(1) << "\n";
+
+    return 0;
 }
 
+std::stringstream printMenu()
+{
+    std::stringstream ssMenu;
+    ssMenu << "============================\n" << "Quadratic equation calculator\n" <<
+        "============================\n";
+    ssMenu << "You have to enter \"a\", \"b\" and \"c\" regarding the equation \n" <<
+        "\ta*(x*x)+b*x+c = 0 \n";
+    return ssMenu;
+}
+
+std::vector<double> inputValues()
+{
+    std::string input;
+    std::vector<double> inputValues;
+    bool legitInput{ false };
+    while(!legitInput)
+    {
+        std::cout << "Enter double values separated with an empty space: ";
+        std::getline(std::cin, input);
+        std::stringstream words(input);
+        while (words >> input)
+        {
+            if (words.str() == "END")
+            {
+                std::cout << "Bye!\n";
+                return inputValues;
+            }
+        }        // TODO Vektor wird falsch befüllt!
+        while (words >> input)
+        {
+            inputValues.push_back(std::stod(words.str()));
+        }
+        if (inputValues.size() != 3)
+        {
+            std::cout << "Wrong number of inputs!\n";
+            continue;
+        }
+        legitInput = true;
+    }
+
+
+    return std::vector<double>();
+}
+
+std::vector<double> outputCalculation(const std::vector<double> values)
+{
+    const double a{ values.at(0) }, b{ values.at(1) }, c{ values.at(2) };
+    const double p{ b / a }, q{ c / a };
+
+    double firstResult{ -(p / 2) + std::sqrt(std::pow((p / 2),2) - q) };
+    double secondResult{ -(p / 2) - std::sqrt(std::pow((p / 2),2) - q) };
+    return std::vector<double>{firstResult, secondResult};
+}
