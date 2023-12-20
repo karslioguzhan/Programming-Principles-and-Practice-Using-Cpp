990c38bdeb716fe89ad9d50fcdea923299fa6eb8
Oguzhan <yakusio@googlemail.com>
2023-12-20 13:24:53 +0100

    Started exercise 18

---
+++ 04_Chapter/18_Exercise/18_Exercise.cpp
@@ -0,0 +1,12 @@
+// 18_Exercise.cpp : Write a program to solve quadratic equations. A quadratic equation is of the form
+// ax2 + bx + c = 0
+// If you don’t know the quadratic formula for solving such an expression, do some research. Remember, researching how to solve a problem is often necessary before a programmer can teach the computer how to solve it. Use doubles for the user inputs for a, b, and c. Since there are two solutions to a quadratic equation, output both x1 and x2.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
