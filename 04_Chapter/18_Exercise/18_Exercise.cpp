9c5169f7da1bddb8b85a03212151dae437a018e8
Oguzhan <yakusio@googlemail.com>
2024-01-02 08:41:49 +0100

    Finished exercise and tested with the values of the wikipedia page

--- 04_Chapter/18_Exercise/18_Exercise.cpp
+++ 04_Chapter/18_Exercise/18_Exercise.cpp
@@ -12,17 +12,18 @@
 std::stringstream printMenu();
 std::vector<double> inputValues();
 std::vector<double> outputCalculation(const std::vector<double> values);
+std::stringstream printResults(std::vector<double>);
 
 int main()
 {
     std::cout << printMenu().str();
     std::vector<double> inputVector{ inputValues() };
-    if (inputVector.size() == 0)
+    if (inputVector.size() != 3)
     {
         return 0;
     }
     std::vector<double> calculationResult{ outputCalculation(inputVector) };
-    std::cout << "The first result of the calculation is: " << calculationResult.at(0) << " \n The second result is: " << calculationResult.at(1) << "\n";
+    std::cout << printResults(calculationResult).str();
 
     return 0;
 }
@@ -33,7 +34,7 @@ std::stringstream printMenu()
     ssMenu << "============================\n" << "Quadratic equation calculator\n" <<
         "============================\n";
     ssMenu << "You have to enter \"a\", \"b\" and \"c\" regarding the equation \n" <<
-        "\ta*(x*x)+b*x+c = 0 \n";
+        "\ta*(x*x)+b*x+c = 0 \nTerminate calculation with \"END\"";
     return ssMenu;
 }
 
@@ -46,19 +47,18 @@ std::vector<double> inputValues()
     {
         std::cout << "Enter double values separated with an empty space: ";
         std::getline(std::cin, input);
+        std::string temp{};
         std::stringstream words(input);
-        while (words >> input)
+        while (words >> temp)
         {
-            if (words.str() == "END")
+            if (temp == "END")
             {
                 std::cout << "Bye!\n";
                 return inputValues;
             }
-        }        // TODO Vektor wird falsch befüllt!
-        while (words >> input)
-        {
-            inputValues.push_back(std::stod(words.str()));
-        }
+            inputValues.push_back(std::stod(temp));
+        }        
+
         if (inputValues.size() != 3)
         {
             std::cout << "Wrong number of inputs!\n";
@@ -66,17 +66,64 @@ std::vector<double> inputValues()
         }
         legitInput = true;
     }
-
-
-    return std::vector<double>();
+    return inputValues;
 }
 
 std::vector<double> outputCalculation(const std::vector<double> values)
 {
     const double a{ values.at(0) }, b{ values.at(1) }, c{ values.at(2) };
     const double p{ b / a }, q{ c / a };
+    const double discriminant{ std::pow(p,2) - 4 * q };
+    double firstResult{}, secondResult{};
+    if (discriminant > 0)
+    {
+        firstResult = -(p / 2) + std::sqrt(std::pow((p / 2),2) - q);
+        secondResult = -(p / 2) - std::sqrt(std::pow((p / 2),2) - q);
+        return std::vector<double>{firstResult, secondResult};
 
-    double firstResult{ -(p / 2) + std::sqrt(std::pow((p / 2),2) - q) };
-    double secondResult{ -(p / 2) - std::sqrt(std::pow((p / 2),2) - q) };
-    return std::vector<double>{firstResult, secondResult};
+    }
+    else if (discriminant == 0)
+    {
+        firstResult = -(p / 2) + std::sqrt(std::pow((p / 2),2) - q);
+        return std::vector<double>{firstResult};
+    }
+    else
+    {
+        double realPart{ -(p / 2) };
+        double complexPart{ std::sqrt(q - std::powf(p / 2, 2)) };
+        return std::vector<double>{realPart, 1, complexPart};
+
+    }
+}
+
+std::stringstream printResults(std::vector<double> calculationResult)
+{
+    const int sizeOfVector{ static_cast<int>(calculationResult.size()) };
+    std::stringstream result;
+    result << "===========================================\n" << "Results\n" <<
+        "===========================================\n";
+
+    switch (sizeOfVector)
+    {
+    case 1:
+    {
+        result << "There is only one intersection with the y axis with the value of x: " <<
+            calculationResult.at(0) << "\n";
+        break;
+    }
+    case 2:
+    {
+        result << "There are two real intersection with the y axis with the value of x1: "
+            << calculationResult.at(0) << " and " << " x2: " << calculationResult.at(1) << "\n";
+        break;
+    }
+    case 3:
+    {
+        result << "There are only two complex intersections with the y axis with the value of x1: " <<
+            calculationResult.at(0) << " + i" << calculationResult.at(2) << "\n and x2: " <<
+            calculationResult.at(0) << " - i" << calculationResult.at(2) << "\n";
+        break;
+    }
+    }
+    return result;
 }
