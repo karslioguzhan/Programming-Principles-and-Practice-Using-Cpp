1e148cc7246d0f685680b298a909e562e8d6eaed
Oguzhan <yakusio@googlemail.com>
2024-02-16 08:26:14 +0100

    Started drill of exercise 6

---
+++ 06_Chapter/01_Drill/01_Drill.cpp
@@ -0,0 +1,231 @@
+// 01_Drill.cpp : This drill involves a series of modifications of a buggy program to turn it
+// from something useless into something reasonably useful.
+//
+//  1. Take the calculator from the file calculator02buggy.cpp. Get it to
+//     compile. You need to find and fix a few bugs. Those bugs are not in the
+//     text in the book. Find the three logic errors deviously inserted in
+//     calculator02buggy.cpp and remove them so the calculator produces correct
+//     results.
+//  2. Change the character used as the exit command from q to x.
+//  3. Change the character used as the print command from ; to =.
+//  4. Add a greeting line in main():
+//      "Welcome to our simple calculator.
+//      Please enter expressions using floating-point numbers."
+//  5. Improve that greeting by mentioning which operators are available an how to print and exit.
+
+//
+// This is example code from Chapter 6.7 "Trying the second version" of
+// "Software - Principles and Practice using C++" by Bjarne Stroustrup
+//
+
+/*
+    This file is known as calculator02buggy.cpp
+
+    I have inserted 5 errors that should cause this not to compile
+    I have inserted 3 logic errors that should cause the program to give wrong results
+
+    First try to find an remove the bugs without looking in the book.
+    If that gets tedious, compare the code to that in the book (or posted source code)
+
+    Happy hunting!
+
+*/
+/*
+    Progress: Resolved all compiler errors. Next step are the logic errors.
+*/
+
+#include "std_lib_facilities.h"
+
+//------------------------------------------------------------------------------
+
+// lass Token{ -> First Error: lass is undefined and should be class
+class Token {
+public:
+    char kind;        // what kind of token
+    double value;     // for numbers: a value 
+    Token(char ch)    // make a Token from a char
+        :kind(ch), value(0) { }
+    Token(char ch, double val)     // make a Token from a char and a double
+        :kind(ch), value(val) { }
+};
+
+//------------------------------------------------------------------------------
+
+class Token_stream {
+public:
+    Token_stream();   // make a Token_stream that reads from cin
+    Token get();      // get a Token (get() is defined elsewhere)
+    void putback(Token t);    // put a Token back
+private:
+    bool full;        // is there a Token in the buffer?
+    Token buffer;     // here is where we keep a Token put back using putback()
+};
+
+//------------------------------------------------------------------------------
+
+// The constructor just sets full to indicate that the buffer is empty:
+Token_stream::Token_stream()
+    :full(false), buffer(0)    // no Token in buffer
+{
+}
+
+//------------------------------------------------------------------------------
+
+// The putback() member function puts its argument back into the Token_stream's buffer:
+void Token_stream::putback(Token t)
+{
+    if (full) error("putback() into a full buffer");
+    buffer = t;       // copy t to buffer
+    full = true;      // buffer is now full
+}
+
+//------------------------------------------------------------------------------
+
+//Token get() --> Error 4 False declaration for function. Should be Token_stream::
+Token Token_stream::get()
+{
+    if (full) {       // do we already have a Token ready?
+        // remove token from buffer
+        full = false;
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+
+    switch (ch) {
+    case ';':    // for "print"
+    case 'q':    // for "quit"
+    case '(': case ')': case '+': case '-': case '*': case '/':
+        return Token(ch);        // let each character represent itself
+    case '.':
+    case '0': case '1': case '2': case '3': case '4':
+    case '5': case '6': case '7': case '9':
+    {
+        cin.putback(ch);         // put digit back into the input stream
+        double val;
+        cin >> val;              // read a floating-point number
+        return Token('8', val);   // let '8' represent "a number"
+    }
+    default:
+        error("Bad token");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+Token_stream ts;        // provides get() and putback() 
+
+//------------------------------------------------------------------------------
+
+double expression();    // declaration so that primary() can call expression()
+
+//------------------------------------------------------------------------------
+
+// deal with numbers and parentheses
+double primary()
+{
+    Token t = ts.get();
+    switch (t.kind) {
+    case '(':    // handle '(' expression ')'
+    {
+        double d = expression();
+        t = ts.get();
+        //if (t.kind != ')') error("')' expected); Second error --> Missing "
+        if (t.kind != ')')
+        {
+            error("')' expected");
+        }
+            return d;
+    }
+    case '8':            // we use '8' to represent a number
+        return t.value;  // return the number's value
+    default:
+        error("primary expected");
+    }
+}
+
+//------------------------------------------------------------------------------
+
+// deal with *, /, and %
+double term()
+{
+    double left = primary();
+    Token t = ts.get();        // get the next token from token stream
+
+    while (true) {
+        switch (t.kind) {
+        case '*':
+            left *= primary();
+            t = ts.get();
+        case '/':
+        {
+            double d = primary();
+            if (d == 0) error("divide by zero");
+            left /= d;
+            t = ts.get();
+            break;
+        }
+        default:
+            ts.putback(t);     // put t back into the token stream
+            return left;
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+// deal with + and -
+double expression()
+{
+    //double left = term(;      // read and evaluate a Term --> Error 3. Missing brace )
+    double left = term();      // read and evaluate a Term
+    Token t = ts.get();        // get the next token from token stream
+
+    while (true) {
+        switch (t.kind) {
+        case '+':
+            left += term();    // evaluate Term and add
+            t = ts.get();
+            break;
+        case '-':
+            left += term();    // evaluate Term and subtract
+            t = ts.get();
+            break;
+        default:
+            ts.putback(t);     // put t back into the token stream
+            return left;       // finally: no more + or -: return the answer
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+
+int main()
+try
+{
+    while (cin) {
+        Token t = ts.get();
+        // 5. Error missing assignment of val;
+        double val = t.value;
+        if (t.kind == 'q') break; // 'q' for quit
+        if (t.kind == ';')        // ';' for "print now"
+            cout << "=" << val << '\n';
+        else
+            ts.putback(t);
+        val = expression();
+    }
+    keep_window_open();
+}
+catch (exception& e) {
+    cerr << "error: " << e.what() << '\n';
+    keep_window_open();
+    return 1;
+}
+catch (...) {
+    cerr << "Oops: unknown exception!\n";
+    keep_window_open();
+    return 2;
+}
+
+//------------------------------------------------------------------------------
