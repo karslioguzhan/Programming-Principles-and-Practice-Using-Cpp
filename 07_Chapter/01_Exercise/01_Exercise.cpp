5bf969287b1181c6a52c2a3faca102dae853987b
Oguzhan <yakusio@googlemail.com>
2024-05-19 08:53:10 +0200

    Reset to working commit

--- 07_Chapter/01_Exercise/01_Exercise.cpp
+++ 07_Chapter/01_Exercise/01_Exercise.cpp
@@ -4,6 +4,7 @@
 	2. Provide an assignment operator, =, so that you can change the value of a variable after you introduce it using let. Discuss why that can be useful and how it can be a source of problems.
 	3. Provide named constants that you really can't change the value of. Hint: You have to add a member to Variable that distinguishes between constants and variables and check for it in set_value(). If you want to let the user define constants (rather than just having pi and e defined as constants), you'll have to add a notation to let the user express that, for example, const pi = 3.14 ;.
 	4. The get_value(), set_value(), is_declared(), and define_name() functions all operate on the variable var_table. Define a class called Symbol_table with a member var_table of type vector<Variable> and member functions get(), set(), is_declared(), and declare(). Rewrite the calculator to use a variable of type Symbol_table.
+
 */
 
 /*
@@ -14,10 +15,7 @@
 */
 
 #include "std_lib_facilities.h"
-#include "Symbol_table.h"
-#include "Token.h"
-#include "Token_stream.h"
-#include "GlobalVariables.h"
+#include <algorithm>
 
 // SquareRoot function prototype
 double squareRootFunction();
@@ -25,22 +23,182 @@ double squareRootFunction();
 // Pow function prototype
 double powFunction();
 
+// Token as struct
+struct Token {
+	// Member variables of Struct
+	char kind;
+	double value;
+	string name;
+	// All Token constructors with initializer list
+	Token(char ch) :kind(ch), value(0) { }
+	Token(char ch, double val) :kind(ch), value(val) { }
+	Token(char ch, string name) :kind(ch), name(name), value(0) { } // 1. Error: Missing constructor
+};
+
+// Token_stream class
+class Token_stream {
+	// Member variables
+	bool full;
+	Token buffer;
+public:
+	// Default Constructor (Not sure if 0 is legit
+	Token_stream() :full(false), buffer(0) { }
+
+	// Method prototype for getting Tokens
+	Token get();
+	// Method for assigning tokens to buffer
+	void unget(Token t) { buffer = t; full = true; }
+	// Method prototype for ignoring inputs
+	void ignore(char);
+};
+
+// Global variables with arbitrary values
+const char let = '#';
+const char quit = 'Q';
+const char print = ';';
+const char number = '8';
+const char name = 'a';
+const char k = 'k';
+const char squareRootVal{ 's' };
+const char powVal{ 'p' };
+
+// Constants
+const string squareRoot{ "sqrt" };
+const string powName{ "pow" };
+
+
+// Getter-Method for Tokens
+Token Token_stream::get()
+{
+	// Returning actual Token if buffer is full
+	if (full) { full = false; return buffer; }
+	// Read input as character
+	char ch{};
+	cin >> ch;
+	// Switch for determining token type
+	switch (ch) {
+	case '(':
+	case ')':
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '%':
+	case ';':
+	case '=':
+	case ',':
+	case let:
+		// non numeric characters
+		return Token(ch);
+	case '.':
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	{
+		// Return numeric values
+		cin.unget();
+		double val;
+		cin >> val;
+		return Token(number, val);
+	}
+	default:
+		// Getting strings from input
+		if (isalpha(ch)) {
+			string s;
+			s += ch;
+			while (cin.get(ch) && (isalpha(ch) || isdigit(ch) || ch == '_')) s += ch; // Logical error -> String was not concatenated right
+			cin.unget();
+			if (s == "#") return Token(let);
+			if (s == "exit") return Token(quit);
+			if (s.size() == 1 && s[0] == k) return Token(number, 1000);
+			if (s == squareRoot) return Token(squareRootVal);
+			if (s == powName) return Token(powVal);
+			return Token(name, s);
+		}
+		// Error message if nothing valid is inserted
+		error("Bad token");
+	}
+}
+
+// Method for ignoring input chars
+void Token_stream::ignore(char c)
+{
+	// Check if buffer is full and kind of buffer
+	if (full && c == buffer.kind) {
+		// Set full to false
+		full = false;
+		return;
+	}
+
+	full = false;
+	char ch;
+	while (cin >> ch)
+		if (ch == c) return;
+}
+
+// Create Variable struct
+struct Variable {
+	string name;
+	double value;
+	bool isConst{};
+	Variable(string n, double v, bool isConst) :name(n), value(v), isConst(isConst) { }
+};
+
+// Initialize empty global vector
+vector<Variable> names{};
+
+// Get string values
+double get_value(string s)
+{
+	// Iterate through vector and check if input string is in names
+	for (int i = 0; i < names.size(); ++i)
+		if (names[i].name == s) return names[i].value;
+	error("get: undefined name ", s);
+}
+
+// Setter for names with string and value
+void set_value(string s, double d)
+{
+	for (int i = 0; i <= names.size(); ++i)
+		if (names[i].name == s) {
+			names[i].value = d;
+			return;
+		}
+	error("set: undefined name ", s);
+}
+
+// Check if string is declared
+int is_declared(string s)
+{
+	for (int i = 0; i < names.size(); ++i)
+		if (names[i].name == s) return i;
+	return -1;
+}
+
+// Token stream as global variable (not best practice)
+Token_stream ts;
+
 // Function prototype for expressions
 double expression();
 
 // get primaries
 double primary()
 {
-	Symbol_table* referenceSymbolTable = Symbol_table::getInstance();
-	Token_stream* ts = Token_stream::GetInstance();
 	// Get token from token-stream
-	Token t = ts->get();
+	Token t = ts.get();
 	// Switch regarding of token kind
 	switch (t.kind) {
 	case '(':
 	{
 		double d = expression();
-		t = ts->get();
+		t = ts.get();
 		if (t.kind != ')') error("')' expected");
 		return d; // Missing return
 	}
@@ -49,7 +207,7 @@ double primary()
 	case number:
 		return t.value;
 	case name:
-		return referenceSymbolTable->get_value(t.name);
+		return get_value(t.name);
 	case squareRootVal:
 		return squareRootFunction();
 	case powVal:
@@ -64,10 +222,9 @@ double term()
 {
 	// Get left primary
 	double left = primary();
-	Token_stream* ts = Token_stream::GetInstance();
 	while (true) {
 		// Get token and check if multiplication or division
-		Token t = ts->get();
+		Token t = ts.get();
 		switch (t.kind) {
 		case '*':
 			// Multiply with primary
@@ -83,7 +240,7 @@ double term()
 		}
 		// Default just return the value
 		default:
-			ts->unget(t);
+			ts.unget(t);
 			return left;
 		}
 	}
@@ -92,10 +249,9 @@ double term()
 // Handle expressions
 double expression()
 {
-	Token_stream* ts = Token_stream::GetInstance();
 	double left = term();
 	while (true) {
-		Token t = ts->get();
+		Token t = ts.get();
 		switch (t.kind) {
 		case '+':
 			left += term();
@@ -104,21 +260,68 @@ double expression()
 			left -= term();
 			break;
 		default:
-			ts->unget(t);
+			ts.unget(t);
 			return left;
 		}
 	}
 }
 
+// Handle declarations
+double declaration()
+{
+	Token t = ts.get();
+	// kind name
+	if (t.kind != 'a') error("name expected in declaration");
+	string name = t.name;
+	// Case for const
+	if (strcmp(name.c_str(), "const") == 0)
+	{
+		Token variableNameToken = ts.get();
+		if (variableNameToken.kind != 'a')
+		{
+			error("name expected in declaration");
+		}
+		string name = variableNameToken.name;
+		Token equalSignToken = ts.get();
+		if (equalSignToken.kind != '=') error("= missing in declaration of ", name);
+		double d = expression();
+		int searchResult{ is_declared(name) };
+		if (searchResult >= 0 && !names.at(searchResult).isConst) // TODO Continue here
+		{
+			names.at(searchResult).value = d;
+			names.at(searchResult).isConst = true;
+			return d;
+		}
+		names.push_back(Variable(name, d, true));
+		return d;
+	}
+	// Default case
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("= missing in declaration of ", name);
+	double d = expression();
+	int searchResult{ is_declared(name) };
+	if (searchResult >= 0 && names.at(searchResult).isConst)
+	{
+		error("Cannot override const variable");
+	}
+	if (searchResult >= 0 && !names.at(searchResult).isConst) // TODO Continue here
+	{
+		names.at(searchResult).value = d;
+		return d;
+	}
+
+	names.push_back(Variable(name, d, false));
+	return d;
+}
+
 // Handle statements
 double statement()
 {
-	Symbol_table* referenceSymbolTable = Symbol_table::getInstance();
 	Token t = ts.get();
 	switch (t.kind) {
-		// distinction between declaration and expression
+		// distincition between declaration and expression
 	case let:
-		return referenceSymbolTable->declaration();
+		return declaration();
 	default:
 		ts.unget(t);
 		return expression();
