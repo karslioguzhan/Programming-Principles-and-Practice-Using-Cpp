6c13b5ae9bcc290b72164c156d30e14b5267f868
Oguzhan <yakusio@googlemail.com>
2024-05-26 16:22:15 +0200

    Finished 1. exercise of drill

---
+++ 08_Chapter/01_Drill/my.cpp
@@ -0,0 +1,12 @@
+#include "my.h"
+#include "std_lib_facilities.h"
+
+void print_foo()
+{
+	std::cout << "Value of foo: " << foo << "\n";
+}
+
+void print(int i)
+{
+	std::cout << "Value of variable i: " << i << "\n";
+}
