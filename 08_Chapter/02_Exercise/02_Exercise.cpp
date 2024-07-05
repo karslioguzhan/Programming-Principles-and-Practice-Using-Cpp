93002f841b31c48079bbcb3d2f4f5fb50a3230b0
okarsli <Oguzhan.Karsli@expleogroup.com>
2024-07-05 23:05:00 +0200

    Start of exercise 12

---
+++ 08_Chapter/02_Exercise/02_Exercise.cpp
@@ -0,0 +1,8 @@
+// 02_Exercise.cpp : Write a function print() that prints a vector of ints to cout. Give it two arguments: a string for "labeling" the output and a vector.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
