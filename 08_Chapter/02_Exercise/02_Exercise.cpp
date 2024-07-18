37901af12810e0be8cafc20f9a67e7737a38a944
Oguzhan <yakusio@googlemail.com>
2024-07-18 16:40:14 +0200

    Finished exercise 2

--- 08_Chapter/02_Exercise/02_Exercise.cpp
+++ 08_Chapter/02_Exercise/02_Exercise.cpp
@@ -1,8 +1,25 @@
 // 02_Exercise.cpp : Write a function print() that prints a vector of ints to cout. Give it two arguments: a string for "labeling" the output and a vector.
 
 #include <iostream>
+#include <sstream>
+#include <vector>
 
+std::stringstream print(std::string label, std::vector<int>& values);
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<int> elements{ 1, 3, 45, 50, 600 };
+    std::string label{ "Random" };
+    std::cout << print(label, elements).str();
+}
+
+std::stringstream print(std::string label, std::vector<int>& values)
+{
+    std::stringstream output;
+    output << label << ":\t";
+    for (auto elementVector:values)
+    {
+        output << " " << elementVector;
+    }
+    output << "\n";
+    return output;
 }
