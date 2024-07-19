893c2869ad523400620b341f8a66c8c32e76654a
Oguzhan <yakusio@googlemail.com>
2024-07-19 18:35:57 +0200

    Finished exercise 7

--- 08_Chapter/07_Exercise/07_Exercise.cpp
+++ 08_Chapter/07_Exercise/07_Exercise.cpp
@@ -12,35 +12,90 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <sstream>
 
 std::vector<std::string> vectorOfNames(bool limited);
-std::vector<double> vectorOfAges(bool limited);
+std::vector<double> vectorOfAges(bool limited, int numOfAges);
+void sortVector(std::vector<std::string>& vectorOfNames, std::vector<double>& vectorOfAges);
+std::stringstream print(std::string label, const std::vector<std::string>& names, const std::vector<double>& ages);
+
 
 int main()
 {
-	vectorOfNames(true);
+	// Restricted to 5 inputs, to allow more inputs set false
+	std::vector<std::string> namesVector{ vectorOfNames(true) };
+	std::vector<double> agesVector{ vectorOfAges(true, namesVector.size()) };
+	std::cout << print("Before sorting", namesVector, agesVector).str();
+	sortVector(namesVector, agesVector);
+	std::cout << print("After sorting", namesVector, agesVector).str();
+	return 0;
 }
 
 std::vector<std::string> vectorOfNames(bool limited)
 {
 	std::vector<std::string> tempVector;
-	int numInput{ 0 }, maxNames{ 2 - 1};
+	int numInput{ 0 }, maxNames{ 5 - 1 };
 	std::string actualName;
 	do
 	{
-		std::cout << "Enter a name: ";
+		std::cout << "Enter a name or \"end\" to end input: ";
 		std::cin >> actualName;
 		numInput++;
-		if (actualName == "\n" || (limited && numInput < maxNames))
+		if (actualName == "end")
 		{
 			break;
 		}
 		tempVector.push_back(actualName);
-	} while (actualName != "\n" || (!limited && numInput < maxNames));
+		if (numInput > maxNames && limited)
+		{
+			break;
+		}
+
+	} while (actualName != "end");
 	return tempVector;
 }
 
-std::vector<double> vectorOfAges(bool limited)
+std::vector<double> vectorOfAges(bool limited, int numOfAges)
 {
-	return std::vector<double>();
+	std::vector<double> tempVector;
+	for (int numIteration{ 0 }; numIteration < numOfAges; ++numIteration)
+	{
+		std::cout << "Enter age: ";
+		double inputAge{};
+		std::cin >> inputAge;
+		tempVector.push_back(inputAge);
+	}
+	return tempVector;
+}
+
+void sortVector(std::vector<std::string>& vectorOfNames, std::vector<double>& vectorOfAges)
+{
+	std::vector<std::string> copyNames{ vectorOfNames };
+	std::vector<double> copyAges{ vectorOfAges };
+	std::sort(vectorOfNames.begin(), vectorOfNames.end());
+	for (int numNames{ 0 }; numNames < copyNames.size(); ++numNames)
+	{
+		std::string searchedName{ copyNames.at(numNames) };
+		// Replace with right one
+		for (int numSortedNames{ 0 }; numSortedNames < vectorOfNames.size(); ++numSortedNames)
+		{
+			if (vectorOfNames.at(numSortedNames) == searchedName)
+			{
+				vectorOfAges.at(numSortedNames) = copyAges.at(numNames);
+				break;
+			}
+		}
+	}
+}
+
+std::stringstream print(std::string label, const std::vector<std::string>& names, const std::vector<double>& ages)
+{
+	std::stringstream output;
+	output << label << ":\n";
+	for (int numElement{ 0 }; numElement < names.size(); ++numElement)
+	{
+		output << "\t" << names.at(numElement) << "\t" << ages.at(numElement) << "\n";
+	}
+	return output;
 }
