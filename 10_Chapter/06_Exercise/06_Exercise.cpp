b0f255812f6d7d6fc87d2b5fd6b821b3cfc8a4b6
Oguzhan <yakusio@googlemail.com>
2024-10-27 16:51:15 +0100

    Progress

--- 10_Chapter/06_Exercise/06_Exercise.cpp
+++ 10_Chapter/06_Exercise/06_Exercise.cpp
@@ -9,12 +9,13 @@
 class Roman_int
 {
 public:
-	Roman_int();
+	Roman_int(int inputRomanNumber);
 	int as_int() const;
-	std::ostream& operator<< (std::ostream& os);
+	//std::ostream& operator<< (std::ostream& os);
 	
 private:
 	std::string romanLiteral;
+	int romanInteger;
 	std::unordered_map<char, int> romanToIntMap
 	{
 			{'I', 1},
@@ -27,25 +28,40 @@ private:
 	};
 };
 
-Roman_int::Roman_int()
+Roman_int::Roman_int(int inputRomanNumber):
+	romanInteger{inputRomanNumber}
 {
+	std::vector<std::string> notation = { "I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M" };
+	std::vector<int> numbers = { 1,4,5,9,10,40,50,90,100,400,500,900,1000 };
+	int i = numbers.size() - 1;
+	std::string tempAnswer;
+
+	while (inputRomanNumber > 0) {
+		while (inputRomanNumber >= numbers[i]) {  // Changed condition here
+			tempAnswer += notation[i];
+			inputRomanNumber -= numbers[i];
+		}
+		--i;
+	}
+	romanLiteral = tempAnswer;
 }
 
 int Roman_int::as_int() const
 {
-	return 0;
+	return romanInteger;
 }
 
-std::ostream& Roman_int::operator<<(std::ostream& os)
-{
-	// TODO: insert return statement here
-}
+//std::ostream& Roman_int::operator<<(std::ostream& os)
+//{
+//	// TODO: insert return statement here
+//}
 
-Roman_int::~Roman_int()
-{
-}
+
 int main()
 {
+	Roman_int test1{ 19 }; // XIX
+	Roman_int test2{ 2540 }; // MMDXL
+
     std::cout << "Hello World!\n";
 }
 
