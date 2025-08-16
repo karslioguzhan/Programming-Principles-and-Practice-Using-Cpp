6a9ed40ecd6b1ee134bb2a835f877a068eda4b3f
karslioguzhan <61474885+karslioguzhan@users.noreply.github.com>
2025-08-16 19:08:36 +0200

    Finished exercise 04

--- 11_Chapter/04_Exercise/04_Exercise.cpp
+++ 11_Chapter/04_Exercise/04_Exercise.cpp
@@ -8,6 +8,8 @@
 //    65      decimal       converts to   65    decimal
 
 #include <cctype>
+#include <iomanip>
+#include <ios>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -36,19 +38,14 @@ enum class TypeOfInt : int
 {
   decimal,
   hexadecimal,
-  octal
+  octal,
+  error
 };
 
 std::pair<bool, std::pair<TypeOfInt, int>> static validateAndConvertInput(
     std::string const &InputString)
 {
-  // Checking for decimal values
   bool CheckIfDecimal{isDecimal(InputString)};
-  if (CheckIfDecimal)
-  {
-    return std::pair<bool, std::pair<TypeOfInt, int>>{true,
-                                                      {TypeOfInt::decimal, std::stoi(InputString)}};
-  }
 
   // Checking for hexadecimal  values
   if (InputString.substr(0, 2).compare("0x") == 0)
@@ -66,37 +63,85 @@ std::pair<bool, std::pair<TypeOfInt, int>> static validateAndConvertInput(
 
   if (InputString.substr(0, 1).compare("0") == 0)
   {
-    bool CheckIfDecimal{isDecimal(InputString.substr(2, InputString.length() - 1))};
+    bool CheckIfDecimal{isDecimal(InputString.substr(1, InputString.length() - 1))};
     if (CheckIfDecimal)
     {
       int TempValue{};
       std::stringstream TempStringStream;
-      TempStringStream << std::hex << InputString;
+      TempStringStream << std::oct << InputString;
       TempStringStream >> TempValue;
-      return std::pair<bool, std::pair<TypeOfInt, int>>{true, {TypeOfInt::hexadecimal, TempValue}};
+      return std::pair<bool, std::pair<TypeOfInt, int>>{true, {TypeOfInt::octal, TempValue}};
     }
   }
+  // Checking for decimal values
+  if (CheckIfDecimal)
+  {
+    return std::pair<bool, std::pair<TypeOfInt, int>>{true,
+                                                      {TypeOfInt::decimal, std::stoi(InputString)}};
+  }
 
+  // Negative return
+  return std::pair<bool, std::pair<TypeOfInt, int>>{false, {TypeOfInt::error, 0}};
+}
 
+auto static collectIntegers()
+{
+  std::vector<std::pair<bool, std::pair<TypeOfInt, int>>> TempCollectedIntegers;
+  std::cout << "Integer Converter. To exit press  \"x\"\n";
+  std::string InputValue{};
+  do
+  {
+    promptInput();
+    std::cin >> InputValue;
+    auto TempValue{validateAndConvertInput(InputValue)};
+    if (TempValue.first)
+    {
+      TempCollectedIntegers.push_back(TempValue);
+    }
+    else if (InputValue.compare("x") != 0)
+    {
+      std::cout << "Error: " << InputValue << " in not an valid value!\n";
+    }
+  } while (InputValue.compare("x") != 0);
+  return TempCollectedIntegers;
+}
 
-  return std::pair<bool, std::pair<TypeOfInt, int>>{true,
-                                                    {TypeOfInt::decimal, std::stoi(InputString)}};
+std::stringstream static convertIntegersToStream(
+    std::vector<std::pair<bool, std::pair<TypeOfInt, int>>> const &CollectedIntegersInput)
+{
+  std::stringstream TempStream;
+  if (CollectedIntegersInput.empty())
+  {
+    TempStream << "No valid values inserted! Nothing to print. \n";
+  }
+  for (auto ActualInteger : CollectedIntegersInput)
+  {
+    switch (ActualInteger.second.first)
+    {
+    case TypeOfInt::decimal:
+      TempStream << std::dec << std::setw(10) << ActualInteger.second.second
+                 << " decimal\tconverts to " << ActualInteger.second.second << " decimal\n";
+      break;
+    case TypeOfInt::hexadecimal:
+      TempStream << std::showbase << std::hex << std::setw(10) << ActualInteger.second.second
+                 << std::setw(10) << " hexadecimal\tconverts to " << std::dec
+                 << ActualInteger.second.second << " decimal\n";
+      break;
+    case TypeOfInt::octal:
+      TempStream << std::showbase << std::oct << std::setw(10) << ActualInteger.second.second
+                 << std::setw(10) << " octal\tconverts to " << std::dec
+                 << ActualInteger.second.second << " decimal\n";
+      break;
+    case TypeOfInt::error:
+      break;
+    }
+  }
+  return TempStream;
 }
 
 int main()
 {
-  std::string InputValue{};
-  std::vector<std::pair<bool, std::pair<TypeOfInt, int>>> CollectedIntegers;
-  promptInput();
-  while (std::cin >> InputValue)
-  {
-    promptInput();
-    auto TempValue{validateAndConvertInput(InputValue)};
-    if (TempValue.first)
-    {
-      CollectedIntegers.push_back(TempValue);
-    }
-  }
-
+  std::vector<std::pair<bool, std::pair<TypeOfInt, int>>> CollectedIntegers{collectIntegers()};
+  std::cout << convertIntegersToStream(CollectedIntegers).str();
   return 0;
 }
